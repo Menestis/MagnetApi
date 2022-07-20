ef72d80356bc1b87a8747638d4d1c59a3120190b
Guillaume ETHEVE <admin@guillaume-etheve.fr>
2022-07-20 22:48:00 +0400

    getPlayerUUID

--- src/main/java/fr/blendman/magnet/api/handles/PlayerHandle.java
+++ src/main/java/fr/blendman/magnet/api/handles/PlayerHandle.java
@@ -1,7 +1,6 @@
 package fr.blendman.magnet.api.handles;
 
 import fr.blendman.skynet.models.PlayerInfo;
-import fr.blendman.skynet.models.PlayerSanction;
 import fr.blendman.skynet.models.PlayerSanctionResult;
 
 import java.util.UUID;
@@ -19,6 +18,9 @@ public interface PlayerHandle {
     CompletableFuture<String> movePlayerToServer(UUID player, UUID server, boolean whitelist);
 
     CompletableFuture<String> movePlayerToServer(UUID player, String serverKind);
+
+    CompletableFuture<UUID> getPlayerUUID(String player);
+
 
     CompletableFuture<PlayerSanctionResult> sanctionPlayer(UUID player, String category, UUID issuer, boolean unsanction);
 }
